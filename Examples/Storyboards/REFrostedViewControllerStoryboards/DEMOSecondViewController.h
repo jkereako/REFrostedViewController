76132f8e3ea9c396842ffbbfef357ef2a7926f0a
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-02-22 13:57:12 -0500

    Replaced #include with @import.

--- Examples/Storyboards/REFrostedViewControllerStoryboards/DEMOSecondViewController.h
+++ Examples/Storyboards/REFrostedViewControllerStoryboards/DEMOSecondViewController.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Roman Efimov. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 #import "REFrostedViewController.h"
 
 @interface DEMOSecondViewController : UIViewController
