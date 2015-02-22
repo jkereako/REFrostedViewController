dc7116c8394b93dfc88cd0c5966fd73a11437723
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-02-22 15:37:43 -0500

    Added the call [super awakeFromNib]

--- Example/REFrostedViewControllerStoryboards/DEMORootViewController.m
+++ Example/REFrostedViewControllerStoryboards/DEMORootViewController.m
@@ -15,6 +15,8 @@
 @implementation DEMORootViewController
 
 - (void)awakeFromNib {
+    [super awakeFromNib];
+    
     self.contentViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"contentController"];
     self.menuViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"menuController"];
 }
